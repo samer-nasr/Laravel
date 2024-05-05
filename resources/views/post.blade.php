e40fe3a5c7f423ebfc14376735f9a814c813aed3
Samer nasr <115714229+samer-nasr@users.noreply.github.com>
2024-05-06 01:53:55 +0300

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/app.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <article>
+
+        <?= $post; ?>
+
+    </article>
+    <a href="/">Go Back</a>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}' , function($slug){
+    
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    
+    if(! file_exists($path)){ 
+        return redirect('/');
+        // abort(404);
+        // dd('File Does not exist!');
+    }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
+
+
+// Route::get('/pizzas', function () {
+//     // return view('pizzas');
+//     return ['name'=> 'samer' , 'gender' => 'male'];
+// });
+
+// Route::get('/samer', function () {
+//     return view('test');
+// });
+
+// Route::get('/home',function(){
+//     return '<h2>Home Page</h2>';
+// });
+
+// Route::get('/test/{id}',function($id){
+//     return 'ID number is:'.$id;
+// });
+
+// Route::get('/test/{id}/{name}',function($id,$name){
+//     return 'ID number is:'.$id."<br> Name : ".$name;
+// });
+
+// //Optional Parameters//
+// Route::get('/test/{id?}',function($id=0){
+//     return 'ID number is:'.$id;
+// });
+
+// //Expression constraints (where)//
+// Route::get('/where/{name?}',function($name=null){
+//     return 'Name is:'.$name;
+// })->where('name','[a-zA-Z]+');
+
+// Route::get('user/{id?}', function ($id=null) {  
+//     return "id is : ". $id;  
+// })->where('id','[0-9]+');
+
+// Route::get('user/{id}/{name}', function ($id,$name) {  
+//     return "id is : ". $id ." ,".  "Name is : ".$name ;  
+// })->where(['id'=>'[0-9]+', 'name'=>'[a-zA-Z]+']);  
+
+
